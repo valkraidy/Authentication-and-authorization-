6aedc043552e590fab82b4a377bb3b743205ea26
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2024-01-14 23:47:27 -1200

    worked on the profile

--- Auth0/src/component/Profile.jsx
+++ Auth0/src/component/Profile.jsx
@@ -4,11 +4,18 @@ import {useAuth0} from '@auth0/auth0-react'
 const Profile = () => {
     const {user, isAuthenticated} = useAuth0();
   return (
-    isAuthenticated && (
+  isAuthenticated && (
         <article>
-            {JSON.Stringify(user)}
+
+            {user?.picture && <img src={user.picture} alt={user.name}/>}
+            <h2>{user?.name}</h2>
+            <ul>
+                {Object.keys(user.map(objKey,1) => <li key={i}>{objKey}:{user[objKey]}</li>)}
+            </ul>
+          
         </article>
     )
+ 
   
   )
 }
